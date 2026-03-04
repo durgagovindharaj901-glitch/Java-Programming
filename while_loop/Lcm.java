6e678bd54974ecac3edff0a37dd23a53f0c53a95
durgagovindharaj901-glitch <durgagovindharaj901@gmail.com>
2026-03-04 23:36:24 +0530

    Add files via upload

---
+++ while_loop/Lcm.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Lcm{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int x = a;
+        int y = b;
+
+        while(y!=0){
+            int temp = y;
+            y = x%y;
+            x = temp;
+        }
+        int gcd = x;
+        int lcm = (a*b)/gcd;
+        System.out.println("LCM of "+ a +" and "+ b + " = "+lcm);
+    }
+}
