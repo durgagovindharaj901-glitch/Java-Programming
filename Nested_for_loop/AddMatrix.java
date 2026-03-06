0ebc25df6b9a572175c5084e5285eb93bce953e0
durgagovindharaj901-glitch <durgagovindharaj901@gmail.com>
2026-03-06 15:45:46 +0530

    Add files via upload

---
+++ Nested_for_loop/AddMatrix.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class AddMatrix{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+        int col = scan.nextInt();
+        int [][]matrixA = new int[row][col];
+        int [][]matrixB = new int[row][col];
+        int [][]result = new int[row][col];
+
+        System.out.println("Matrix A: ");
+        for(int i = 0;i<row;i++){
+            for(int j=0;j<col;j++){
+                matrixA[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println("Matrix B: ");
+        for(int i = 0;i<row;i++){
+            for(int j=0;j<col;j++){
+                matrixB[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println("Sum(A+B): ");    
+        for(int i = 0;i<row;i++){
+            for(int j=0;j<col;j++){
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+                System.out.print(result[i][j] + " "); 
+            }
+            
+            
+            System.out.println();
+        }
+    }
+}
