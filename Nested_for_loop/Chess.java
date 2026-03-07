30106dd7087e3174070907fa42a4419747cabd5b
durgagovindharaj901-glitch <durgagovindharaj901@gmail.com>
2026-03-07 23:34:14 +0530

    Add files via upload

---
+++ Nested_for_loop/Chess.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Chess{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if ((row + col) % 2 == 0) {
+                    System.out.print("■ ");
+                } else {
+                    System.out.print("□ ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
