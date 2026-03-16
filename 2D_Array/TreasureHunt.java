3ff6e20ed54909a7195278bb30877cd5404fd10a
durgagovindharaj901-glitch <durgagovindharaj901@gmail.com>
2026-03-16 22:09:38 +0530

    Add files via upload

---
+++ 2D_Array/TreasureHunt.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class TreasureHunt{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+        int col = scan.nextInt();
+        int[][] T = new int[row][col];
+        for(int i =0;i<row;i++){
+            for(int j =0;j<col;j++){
+                T[i][j] = scan.nextInt();
+            }
+        }
+        int row = scan.nextInt();
+        int col = scan.nextInt();
+        int[][] C = new int[row][col];
+        for(int i =0;i<row;i++){
+            for(int j =0;j<col;j++){
+                C[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println("Treasure Map:"); 
+        map[row][col] =     }
+}
