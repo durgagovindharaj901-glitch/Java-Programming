e9b33267edfdfcaca042718e2441466438ca19c3
durgagovindharaj901-glitch <durgagovindharaj901@gmail.com>
2026-03-17 10:11:11 +0530

    Add files via upload

--- 2D_Array/TreasureHunt.java
+++ 2D_Array/TreasureHunt.java
@@ -2,22 +2,34 @@ import java.util.Scanner;
 class TreasureHunt{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int row = scan.nextInt();
-        int col = scan.nextInt();
-        int[][] T = new int[row][col];
-        for(int i =0;i<row;i++){
-            for(int j =0;j<col;j++){
-                T[i][j] = scan.nextInt();
+        int[][] map = new int[10][10];
+        int T = scan.nextInt();
+        for(int i =0;i<T;i++){
+            int row = scan.nextInt();
+            int col = scan.nextInt();
+            map[row][col] = 1;
+        }
+        int C = scan.nextInt();
+        for(int i = 0; i<C;i++){
+            int checkR = scan.nextInt();
+            int checkC = scan.nextInt();
+            System.out.print("Checking Location (" + checkR + ", "+checkC + "): ");
+            if(map[checkR][checkC] == 1){
+                System.out.println("Treasure Found!");
+                map[checkR][checkC] = 0;
+            }else{
+                System.out.println("No Treasure");
             }
         }
-        int row = scan.nextInt();
-        int col = scan.nextInt();
-        int[][] C = new int[row][col];
-        for(int i =0;i<row;i++){
-            for(int j =0;j<col;j++){
-                C[i][j] = scan.nextInt();
+        System.out.println("Final Map:");
+        printMap(map);
+    }
+    private static void printMap(int[][] map){
+        for(int i = 0; i< map.length;i++){
+            for(int j =0;j< map[i].length ;j++){
+                System.out.print(map[i][j] + " ");
             }
+            System.out.println();
         }
-        System.out.println("Treasure Map:"); 
-        map[row][col] =     }
+    }
 }
